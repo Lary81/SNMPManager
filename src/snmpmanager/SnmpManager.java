7a65a4b8004a35adc08d2151dca4ffe560e2f882
Portal <Portal@info-pedro>
2015-04-09 14:04:46 -0300

    Início da implementação do network discovery com snmp4j.

--- src/snmpmanager/SnmpManager.java
+++ src/snmpmanager/SnmpManager.java
@@ -1,12 +1,70 @@
 package snmpmanager;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.snmp4j.CommunityTarget;
+import org.snmp4j.PDU;
+import org.snmp4j.ScopedPDU;
+import org.snmp4j.Session;
+import org.snmp4j.Snmp;
+import org.snmp4j.Target;
+import org.snmp4j.TransportMapping;
+import org.snmp4j.UserTarget;
+import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.event.ResponseListener;
+import org.snmp4j.mp.MPv3;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.security.SecurityLevel;
+import org.snmp4j.security.SecurityModels;
+import org.snmp4j.security.SecurityProtocols;
+import org.snmp4j.security.USM;
+import org.snmp4j.smi.Address;
+import org.snmp4j.smi.GenericAddress;
+import org.snmp4j.smi.IpAddress;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.VariableBinding;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.DefaultPDUFactory;
+import org.snmp4j.util.TableEvent;
+import org.snmp4j.util.TableUtils;
+
 /**
+ * Simplest client possible
  *
- * @author Mateus Athaydes
+ * @author johanrask
+ *
  */
 public class SnmpManager {
-    
-    public static void main(String[] args) {
-        System.out.println("Hey!");
+
+    public static void main(String[] args) throws IOException {
+
+        PDU pdu = new PDU();
+        pdu.add(new VariableBinding(new OID(new int[]{1, 3, 6, 1, 2, 1, 1, 1})));
+        pdu.add(new VariableBinding(new OID(new int[]{1, 3, 6, 1, 2, 1, 1, 2})));
+        pdu.setType(PDU.GETNEXT);
+        
+        Address targetAddress = GenericAddress.parse("udp:255.255.255.255/161");
+        CommunityTarget target = new CommunityTarget();
+        target.setCommunity(new OctetString("public"));
+        target.setAddress(targetAddress);
+        target.setRetries(1);
+        target.setTimeout(5000);
+        target.setVersion(SnmpConstants.version1);
+        
+        try {
+            Snmp snmp = new Snmp(new DefaultUdpTransportMapping());
+            snmp.listen();
+            ResponseEvent response = snmp.send(pdu, target);
+            if (response.getResponse() != null) {
+                System.out.println(response.getPeerAddress());
+                System.out.println(response.getResponse());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(99);
+        }
     }
 }
