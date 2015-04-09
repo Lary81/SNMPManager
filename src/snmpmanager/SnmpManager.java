b45ba89bc251e6d32a181674c3e74ed562585032
Portal <Portal@info-pedro>
2015-04-09 14:30:36 -0300

    (fix) Início da implementação do network discovery com snmp4j.

--- src/snmpmanager/SnmpManager.java
+++ src/snmpmanager/SnmpManager.java
@@ -1,42 +1,17 @@
 package snmpmanager;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.snmp4j.CommunityTarget;
 import org.snmp4j.PDU;
-import org.snmp4j.ScopedPDU;
-import org.snmp4j.Session;
 import org.snmp4j.Snmp;
-import org.snmp4j.Target;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.UserTarget;
 import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.event.ResponseListener;
-import org.snmp4j.mp.MPv3;
 import org.snmp4j.mp.SnmpConstants;
-import org.snmp4j.security.SecurityLevel;
-import org.snmp4j.security.SecurityModels;
-import org.snmp4j.security.SecurityProtocols;
-import org.snmp4j.security.USM;
 import org.snmp4j.smi.Address;
 import org.snmp4j.smi.GenericAddress;
-import org.snmp4j.smi.IpAddress;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
-import org.snmp4j.util.DefaultPDUFactory;
-import org.snmp4j.util.TableEvent;
-import org.snmp4j.util.TableUtils;
 
-/**
- * Simplest client possible
- *
- * @author johanrask
- *
- */
 public class SnmpManager {
 
     public static void main(String[] args) throws IOException {
